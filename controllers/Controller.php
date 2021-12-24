8f31272d961af61a5369529646cb69e21bfd9769
Egor <eqour@yandex.ru>
2021-12-24 17:59:26 +0400

    Create view template

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -11,6 +11,16 @@ class Controller {
 
     protected function render(string $view, array $params = []): void {
         extract($params, EXTR_SKIP);
+        ob_start();
         include(Application::APPLICATION_ROOT . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . strtolower($this->name()) . DIRECTORY_SEPARATOR . $view . '.php');
+        $content = ob_get_contents();
+        ob_end_clean();
+        $this->renderLayout('main', ['content' => $content]);
+    }
+
+    private function renderLayout(string $layout, array $params = []): void {
+        extract($params, EXTR_SKIP);
+        ob_start();
+        include(Application::APPLICATION_ROOT . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'layout' . DIRECTORY_SEPARATOR . $layout . '.php');
     }
 }
