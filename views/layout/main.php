9fa63f985a7a402447effb7c59b0c0137e386983
Egor <eqour@yandex.ru>
2022-01-12 20:28:55 +0400

    Update main layout

--- views/layout/main.php
+++ views/layout/main.php
@@ -2,11 +2,27 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable: no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Application</title>
 </head>
 <body>
-    <?= $content ?>
+    <header class="d-flex align-items-center px-1">
+        <img id="logo" src="img/logo.svg">
+    </header>
+    <div class="main row">
+        <div class="col-auto nav" style="background-color: darkgrey;">
+
+        </div>
+        <div class="col-auto">
+            <main>
+                <div class="container">
+                    <?= $content ?>
+                </div>
+            </main>
+        </div>
+    </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
