4624dca895f4b9e1c212f58beb833a44d4328411
Egor <eqour@yandex.ru>
2022-01-13 15:43:56 +0400

    Add a customer search form template

--- views/customer/search.php
+++ views/customer/search.php
@@ -1,1 +1,11 @@
 <h1>Поиск клиента</h1>
+
+<form class="form">
+    <div class="mb-3">
+        <label class="form-label">Серия и номер паспорта</label>
+        <input class="form-control" type="text" maxlength="10">
+    </div>
+    <div class="mb-3">
+        <button class="btn btn-secondary">Найти</button>
+    </div>
+</form>
