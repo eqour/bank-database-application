77c430318f0fc60ff417a163747e505fe0efdbc8
Egor <eqour@yandex.ru>
2022-02-23 18:15:06 +0400

    Add blocking page caching

--- web/index.php
+++ web/index.php
@@ -2,5 +2,7 @@
 
 use app\application\Application;
 
+header('Cache-Control: no-cache, no-store, must-revalidate');
+
 require '..' . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . 'Application.php';
 Application::run();
